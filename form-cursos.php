7b70a078b5cb6395b690ea643df64a35af900add
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-30 15:24:48 -0300

    Create form-cursos.php

---
+++ form-cursos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiCursos</title>
+</head>
+<body>
+    <form action="enviar-form-cursos.php" method="get">
+        digite o nome do curso <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição do curso <br>
+        <input type="text" name="descricao"> <br>
+        digite a carga do curso <br>
+        <input type="number" name="carga"> <br>
+        digite o total de aulas do curso <br>
+        <input type="number" name="totaulas"> <br>
+        digite o preço do curso <br>
+        <input type="text" name="preco"> <br>
+        digite a playlist do curso <br>
+        <input type="text" name="playlist"> <br>
+        digite a imagem do curso <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
